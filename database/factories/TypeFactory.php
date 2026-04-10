01fdcd9408a6e3bfe5290adc8242cbcec12a829f
Paul Longo <paullongo@outlook.com>
2026-04-10 19:04:39 -0400

    Added Type model, factory, migration

---
+++ database/factories/TypeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Type;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Type>
+ */
+class TypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'description' => fake()->optional()->sentence(),
+            'slug' => fake()->unique()->slug(),
+        ];
+    }
+}
